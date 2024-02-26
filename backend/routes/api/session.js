3da76e4ef48879abf6200ee892a93d8f3802d512
Jiamin Gao <tinabb1102@gmail.com>
2024-02-26 00:20:13 -0800

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -46,5 +46,14 @@ router.post(
   }
 );
 
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 
 module.exports = router;
