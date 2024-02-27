7f06bf2f97731e6a01c19df3df8fdc904c2ad785
Jiamin Gao <tinabb1102@gmail.com>
2024-02-27 13:52:21 -0800

    add two columns to users

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -36,7 +36,21 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [60, 60]
         }
-      }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          len: [1, 30]
+        }
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+          len: [1, 30]
+        }
+      },
     },
     {
       sequelize,
--- backend/db/seeders/20240224000809-demo-user.js
+++ backend/db/seeders/20240224000809-demo-user.js
@@ -17,17 +17,24 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName:'Demo',
+        lastName:'User0'
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName:'Fake1',
+        lastName:'User1'
+
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
+        hashedPassword: bcrypt.hashSync('password3'),
+        firstName:'Fake2',
+        lastName:'User2'
       }
     ], { validate: true });
   },
